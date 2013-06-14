b47f705ec32473429aba04bbe0145664d358a9e8
frankh <frankhamand@gmail.com>
2013-06-14 18:29:02 +0200

    Update profiling.py

--- profiling.py
+++ profiling.py
@@ -1,29 +1,23 @@
 def profile(func):
-	import cProfile as profile
-	from StringIO import StringIO
-	import pstats
-	import functools
+        import cProfile as profile
+        import pstats
+        import functools
 
-	@functools.wraps(func)
-	def wrapper(*args, **kwargs):
-		pr = profile.Profile()
-		pr.enable()
-		
-		ret = func(*args, **kwargs)
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+                pr = profile.Profile()
+                pr.enable()
+                
+                ret = func(*args, **kwargs)
 
-		pr.disable()
-		s = StringIO()
-		ps = pstats.Stats(pr, stream=s)
-		ps.sort_stats('time')
-		ps.print_stats(30)
+                pr.disable()
+                ps = pstats.Stats(pr)
+                ps.sort_stats('time')
+                ps.print_stats(20)
 
-		ps.sort_stats('cumulative')
-		ps.print_stats(10)
+                ps.sort_stats('cumulative')
+                ps.print_stats(10)
 
-		s.seek(0)
-		for l in s.readlines():
-			print '@@@',l,
+                return ret
 
-		return ret
-
-	return wrapper
+        return wrapper
