c784c78fa2af6b088ca73f83650336ff48930f8b
frankh <frankhamand@gmail.com>
2013-06-14 18:26:13 +0200

    Create profiling3.py

---
+++ profiling3.py
@@ -0,0 +1,25 @@
+def profile(func):
+  import profile
+	from io import StringIO
+	import pstats
+	import functools
+
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		pr = profile.Profile()
+		pr.enable()
+		
+		ret = func(*args, **kwargs)
+
+		pr.disable()
+		s = StringIO()
+		ps = pstats.Stats(pr)
+		ps.sort_stats('time')
+		ps.print_stats(20)
+
+		ps.sort_stats('cumulative')
+		ps.print_stats(10)
+
+		return ret
+
+	return wrapper
